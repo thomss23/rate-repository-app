81499ec85289a308e21e0971f3dc29ba580d44ae
thomasborbeli <borbelithomas@gmail.com>
2023-03-03 20:07:58 +0200

    ex 10.3

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Main from './src/components/Main';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Thomas</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  console.log("test")
+  return <Main/>
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
