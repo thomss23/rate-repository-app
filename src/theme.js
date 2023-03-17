6ef8653f5ac13b4f4e1d9b8b3f92468b62b26ace
thomasborbeli <borbelithomas@gmail.com>
2023-03-17 18:53:31 +0200

    finish 10.10

--- src/theme.js
+++ src/theme.js
@@ -1,3 +1,5 @@
+import { Platform } from 'react-native';
+
 const theme = {
     colors: {
       textPrimary: '#24292e',
@@ -10,7 +12,7 @@ const theme = {
       subheading: 16,
     },
     fonts: {
-      main: 'System',
+      main: Platform.OS === 'android' ? 'Roboto' : Platform.OS === 'ios' ? 'Arial' : 'System',
     },
     fontWeights: {
       normal: '400',
