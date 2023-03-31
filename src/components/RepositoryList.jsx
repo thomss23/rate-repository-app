d29633e5c285475ddd668a7d2ed055ff57eb64d8
thomasborbeli <borbelithomas@gmail.com>
2023-03-31 16:44:54 +0300

    ex 10.24

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,20 +1,62 @@
+// import Text from './Text';
+// import useRepositories from '../hooks/useRepositories';
+// import { RepositoryListContainer } from './RepositoryListContainer';
+// import { useState } from 'react';
+
+// const RepositoryList = () => {
+//   const [orderBy, setOrderBy] = useState('CREATED_AT');
+//   const [orderDirection, setOrderDirection] = useState('DESC');
+//   const { repositories, loading, error } = useRepositories(orderBy, orderDirection);
+
+//   const [searchKeyword, setSearchKeyword] = useState('');
+//   const [debouncedSearchKeyword] = useDebounce(searchKeyword, 500);
+
+//   if (loading) {
+//     return <Text>Loading...</Text>
+//   }
+//   if (error) {
+//     console.log(error)
+//     return <Text>Error</Text>
+//   }
+
+//   const handleOrderChange = (value) => {
+//     if (value === "CREATED_AT") {
+//       setOrderBy("CREATED_AT");
+//       setOrderDirection("DESC");
+//     } else if (value === "RATING_AVERAGE_ASC") {
+//       setOrderBy("RATING_AVERAGE");
+//       setOrderDirection('ASC');
+//     } else if (value === "RATING_AVERAGE_DESC") {
+//       setOrderBy("RATING_AVERAGE");
+//       setOrderDirection('DESC');
+//     }
+//   };
+//   return <RepositoryListContainer repositories={repositories} orderBy={orderBy} handleOrderChange={handleOrderChange} />;
+// };
+
+// export default RepositoryList;
+
+
 import Text from './Text';
 import useRepositories from '../hooks/useRepositories';
 import { RepositoryListContainer } from './RepositoryListContainer';
 import { useState } from 'react';
+import { useDebounce } from 'use-debounce';
+import { useNavigate } from 'react-router-native';
 
 const RepositoryList = () => {
   const [orderBy, setOrderBy] = useState('CREATED_AT');
   const [orderDirection, setOrderDirection] = useState('DESC');
-  const { repositories, loading, error } = useRepositories(orderBy, orderDirection);
+  
+  const [searchKeyword, setSearchKeyword] = useState('');
+  const [debouncedSearchKeyword] = useDebounce(searchKeyword, 500);
+  const navigate = useNavigate();
 
-  if (loading) {
-    return <Text>Loading...</Text>
-  }
-  if (error) {
-    console.log(error)
-    return <Text>Error</Text>
-  }
+  const { repositories, loading, error } = useRepositories(orderBy, orderDirection, debouncedSearchKeyword);
+
+  const navigateToSingleViewRepository = (id) => {
+    navigate(`/repository/${id}`);
+  };
 
   const handleOrderChange = (value) => {
     if (value === "CREATED_AT") {
@@ -28,7 +70,20 @@ const RepositoryList = () => {
       setOrderDirection('DESC');
     }
   };
-  return <RepositoryListContainer repositories={repositories} orderBy={orderBy} handleOrderChange={handleOrderChange} />;
+
+  if (loading) {
+    return <Text>Loading...</Text>
+  }
+  if (error) {
+    console.log(error)
+    return <Text>Error</Text>
+  }
+
+  return (
+    <>
+      <RepositoryListContainer navigateToSingleViewRepository={navigateToSingleViewRepository} repositories={repositories} orderBy={orderBy} handleOrderChange={handleOrderChange} searchKeyword={searchKeyword} setSearchKeyword={setSearchKeyword} />
+    </>
+  );
 };
 
 export default RepositoryList;
--- src/components/RepositoryListContainer.jsx
+++ src/components/RepositoryListContainer.jsx
@@ -1,46 +1,114 @@
-import { FlatList, Pressable, StyleSheet, View } from "react-native";
-import RepositoryItem from "./RepositoryItem";
-import { useNavigate } from "react-router-native";
-import { Picker } from '@react-native-picker/picker';
+// import { FlatList, Pressable, StyleSheet, View } from "react-native";
+// import RepositoryItem from "./RepositoryItem";
+// import { useNavigate } from "react-router-native";
+// import { Picker } from '@react-native-picker/picker';
 
+
+// const styles = StyleSheet.create({
+//     separator: {
+//       height: 10,
+//       backgroundColor: 'rgb(230, 230, 230)'
+//     },
+// });
+
+// const ItemSeparator = () => <View style={styles.separator} />;
+
+// export const RepositoryListContainer = ({ repositories, orderBy, handleOrderChange }) => {
+//   const repositoryNodes = repositories
+//     ? repositories.edges.map((edge) => edge.node)
+//     : [];
+//   const navigate = useNavigate();
+
+//   const navigateToSingleViewRepository = (id) => {
+//     navigate(`/repository/${id}`)
+//   }
+//   console.log(orderBy)
+//   return (
+//       <FlatList
+//       data={repositoryNodes}
+//       ItemSeparatorComponent={ItemSeparator}
+//       renderItem={({ item: repo }) => {
+//         return(
+//         <Pressable onPress={() => navigateToSingleViewRepository(repo.id)}>
+//           <RepositoryItem repository={repo}/>
+//         </Pressable>
+//         );
+//       }}
+//       ListHeaderComponent={() => 
+//       <Picker selectedValue={orderBy} onValueChange={handleOrderChange}>
+//         <Picker.Item label="Latest repositories" value="CREATED_AT" />
+//         <Picker.Item label="Highest rated repositories" value="RATING_AVERAGE_DESC" />
+//         <Picker.Item label="Lowest rated repositories" value="RATING_AVERAGE_ASC" />
+//       </Picker>}
+//       keyExtractor={repo => repo.id}
+//     />
+//   );
+// };
+
+
+import React from 'react';
+import { FlatList, Pressable, StyleSheet, View } from 'react-native';
+import RepositoryItem from './RepositoryItem';
+import { Picker } from '@react-native-picker/picker';
+import TextInput from './TextInput';
 
 const styles = StyleSheet.create({
-    separator: {
-      height: 10,
-      backgroundColor: 'rgb(230, 230, 230)'
-    },
+  separator: {
+    height: 10,
+    backgroundColor: 'rgb(230, 230, 230)'
+  }, 
+  input: {
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+    margin: 10,
+  },
 });
 
 const ItemSeparator = () => <View style={styles.separator} />;
 
-export const RepositoryListContainer = ({ repositories, orderBy, handleOrderChange }) => {
-  const repositoryNodes = repositories
-    ? repositories.edges.map((edge) => edge.node)
-    : [];
-  const navigate = useNavigate();
-
-  const navigateToSingleViewRepository = (id) => {
-    navigate(`/repository/${id}`)
+export class RepositoryListContainer extends React.Component {
+  constructor(props) {
+    super(props);
   }
-  console.log(orderBy)
-  return (
+  renderHeader = () => {
+    const { orderBy, handleOrderChange, searchKeyword, setSearchKeyword } = this.props;
+    return (
+      <View>
+        <TextInput style={styles.input}
+          label="Search"
+          value={searchKeyword}
+          onChangeText={(text) => {
+            setSearchKeyword(text);
+          }}
+        />
+        <Picker selectedValue={orderBy} onValueChange={handleOrderChange}>
+          <Picker.Item label="Latest repositories" value="CREATED_AT" />
+          <Picker.Item label="Highest rated repositories" value="RATING_AVERAGE_DESC" />
+          <Picker.Item label="Lowest rated repositories" value="RATING_AVERAGE_ASC" />
+        </Picker>
+      </View>
+    );
+  };
+
+  render() {
+    const { repositories, navigateToSingleViewRepository} = this.props;
+    const repositoryNodes = repositories ? repositories.edges.map((edge) => edge.node) : [];
+
+    return (
       <FlatList
-      data={repositoryNodes}
-      ItemSeparatorComponent={ItemSeparator}
-      renderItem={({ item: repo }) => {
-        return(
-        <Pressable onPress={() => navigateToSingleViewRepository(repo.id)}>
-          <RepositoryItem repository={repo}/>
-        </Pressable>
-        );
-      }}
-      ListHeaderComponent={() => 
-      <Picker selectedValue={orderBy} onValueChange={handleOrderChange}>
-        <Picker.Item label="Latest repositories" value="CREATED_AT" />
-        <Picker.Item label="Highest rated repositories" value="RATING_AVERAGE_DESC" />
-        <Picker.Item label="Lowest rated repositories" value="RATING_AVERAGE_ASC" />
-      </Picker>}
-      keyExtractor={repo => repo.id}
-    />
-  );
-};
+        data={repositoryNodes}
+        ItemSeparatorComponent={ItemSeparator}
+        renderItem={({ item: repo }) => {
+          return (
+            <Pressable onPress={() => navigateToSingleViewRepository(repo.id)}>
+              <RepositoryItem repository={repo} />
+            </Pressable>
+          );
+        }}
+        ListHeaderComponent={this.renderHeader}
+        keyExtractor={(repo) => repo.id}
+      />
+    );
+  }
+}
